4a8551a2da7e0d3436083abba8c12f82b2f0cb14
Marcos Vinicius Souza da Costa <marcosmvs105@gmail.com>
2026-05-22 20:21:09 -0500

    aula do dia 22/05/2025 sobre JavaScript

---
+++ fundamentos-de-programação_front-end/exercicios/javascript/script.js
@@ -0,0 +1,97 @@
+// console.log("Hello, World")
+console.log("Boa noite! Hoje é sexta-feira.")
+
+//console
+console.error("Este é um Erro")
+console.warn("Este é um aviso")
+
+//variaveis
+
+var x = 10
+
+//variaveis modernas
+
+let y = 15
+
+const z = 20
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+
+//tipos de dados
+
+//texto
+
+const name = "MARCOS VINICIUS SOUZA DA COSTA"
+
+console.log(name)
+
+console.log(typeof name)
+
+//numerico
+
+const shirtsQtd = 4
+
+console.log(shirtsQtd)
+
+console.log(typeof shirtsQtd)
+
+//numero decimal
+
+
+const decimal = 20.1
+
+console.log(decimal)
+
+console.log(typeof decimal)
+
+//boleano (true e false)
+
+const isAproved = false
+
+console.log(isAproved)
+
+console.log(typeof isAproved)
+
+//null
+let surname = null
+
+console.log(surname)
+
+console.log(typeof surname)
+
+//Array
+let languages = ["JavaScript", "PHP", "Python"]
+
+console.log(languages)
+
+console.log(typeof languages)
+
+//Obmject
+const user = {email: "marcosmvs105@gmail.com", password: "12345", age:"21"}
+
+console.log(user)
+
+console.log(typeof user)
+
+//metodo de string
+
+const fullName = "Marcos Vinicius Souza da Costa"
+
+console.log(fullName.length)
+
+console.log(typeof fullName)
+
+//lista 
+
+const stringToArray = fullName.split(" ")
+
+console.log(fullName.stringToArray)
+
+console.log(stringToArray)
+
+console.log(typeof stringToArray)
+
+console.log(fullName.toLowerCase())
