ee113fa88b1f0b82644d2055a285eb8a9f9a989b
Marcos Vinicius Souza da Costa <marcosmvs105@gmail.com>
2026-05-28 19:47:04 -0500

    atividades do dia 28/05/2026

---
+++ fundamentos-de-programação-back-end/Java/src/Repete.java
@@ -0,0 +1,11 @@
+public class Repete{
+    public static void main(String[] args){
+        // While (enquanto)
+        // While (<condição>) {}
+        int contador = 0;
+        while( contador < 3 ){
+            System.out.println("Olá");
+            contador++;
+        }
+    }
+}
